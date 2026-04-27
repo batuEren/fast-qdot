cdac1cfa6ad094ed5dea0e0a79afedcda3f8604b
Batu Eren <batu.eren2004@gmail.com>
2026-04-27 23:31:59 +0200

    init

---
+++ fast-qdot.cpp
@@ -0,0 +1,12 @@
+﻿// fast-qdot.cpp : Defines the entry point for the application.
+//
+
+#include "fast-qdot.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}

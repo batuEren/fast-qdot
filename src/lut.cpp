0c762ce1237d178886f430f79dec29cc0bc35ff5
Batu Eren <batu.eren2004@gmail.com>
2026-05-08 19:29:14 +0200

    n dim arrays

--- src/lut.cpp
+++ src/lut.cpp
@@ -5,10 +5,28 @@
 
 constexpr int LUT_SIZE = 3;
 
+template<typename T, std::size_t Size, std::size_t Capacity> // capacity is 3 for ternary and 2 for binary
+struct LUT {
+	using type = std::array<typename LUT<T, Size - 1, Capacity>::type, Capacity>;
+};
+
+template<typename T, std::size_t Capacity>
+struct LUT<T, 0, Capacity> {
+	using type = T;
+};
+
+
+
 template<int N>
 constexpr auto lut_initializer() {
-	std::array<uint8_t, N> LUT{};
+	typename LUT<uint8_t, N, 3>::type lut{};
+
+	lut[2][2][2] = 10;
+
+	return 1;
 }
+
+constexpr int i = lut_initializer<3>();
 
 
 int32_t lut_ternary_dot(const uint8_t* weights, const int8_t* activations, int n) {
